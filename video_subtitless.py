ee5989b9fc23dae05c809174c9364fa2745a2d08
Murtaza <murtazaalichang67@gmail.com>
2022-04-23 15:26:29 +0500

    add inpute field

--- video_subtitless.py
+++ video_subtitless.py
@@ -1,10 +1,14 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
-srt = YouTubeTranscriptApi.get_transcript('kqtD5dpn9C8')
+
+video_id = input("Enter Video ID")
+srt = YouTubeTranscriptApi.get_transcript(video_id)
+
+
 
 f = open('srt_file.txt', 'w')
 for i in srt:
-    f.write('%s\n' % i)
+    f.write('%s\n' % i['text'])
 
 
 
